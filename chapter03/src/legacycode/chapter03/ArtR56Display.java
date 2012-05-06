2797e5e6ffbf775d1edef5bc5eb2159730e7789a
nomadmonad <nomadmonad@gmail.com>
2012-05-06 15:47:48 +0900

    modify Sale.java

--- chapter03/src/legacycode/chapter03/ArtR56Display.java
+++ chapter03/src/legacycode/chapter03/ArtR56Display.java
@@ -1,8 +1,12 @@
 package legacycode.chapter03;
 
-public class ArtR56Display {
+public class ArtR56Display implements Display {
 
 	private String line;
+	/* (non-Javadoc)
+	 * @see legacycode.chapter03.Display#showLine(java.lang.String)
+	 */
+	@Override
 	public void showLine(String line) {
 		this.line = line;
 		//‰½‚©‚µ‚ç“n‚³‚ê‚½•¶š—ñ‚ğ•\¦‚·‚éB
--- chapter03/src/legacycode/chapter03/Sale.java
+++ chapter03/src/legacycode/chapter03/Sale.java
@@ -2,10 +2,14 @@ package legacycode.chapter03;
 
 public class Sale {
 
-	private ArtR56Display display;
+	private Display display;
 	public void scan(String barcode) {
 		//バーコードを読み取り、品物の名前と価格を表示する
-		//表示する部分をArtR56Displayに委譲する
+		//ArtR56Displayからインターフェースを抽出したので、インスタンス変数の型をDisplay型に変更する。
 		display.showLine(barcode);
+	}
+	
+	public String asDisplayText(int price) {
+		return Integer.toString(price);
 	}
 }
