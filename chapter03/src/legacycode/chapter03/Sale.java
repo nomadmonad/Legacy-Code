a9798c6b0b1267e9484c59ed5c8220f9ce1d4d2f
nomadmonad <nomadmonad@gmail.com>
2012-05-06 15:39:20 +0900

    modified scan()

--- chapter03/src/legacycode/chapter03/Sale.java
+++ chapter03/src/legacycode/chapter03/Sale.java
@@ -2,7 +2,10 @@ package legacycode.chapter03;
 
 public class Sale {
 
+	private ArtR56Display display;
 	public void scan(String barcode) {
 		//バーコードを読み取り、品物の名前と価格を表示する
+		//表示する部分をArtR56Displayに委譲する
+		display.showLine(barcode);
 	}
 }
