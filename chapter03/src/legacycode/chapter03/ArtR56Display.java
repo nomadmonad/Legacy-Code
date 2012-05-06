80c2d0142e9f6f83f973fbab351f89e2780e13d4
nomadmonad <nomadmonad@gmail.com>
2012-05-06 15:37:45 +0900

    add ArtR56Display.java

---
+++ chapter03/src/legacycode/chapter03/ArtR56Display.java
@@ -0,0 +1,11 @@
+package legacycode.chapter03;
+
+public class ArtR56Display {
+
+	private String line;
+	public void showLine(String line) {
+		this.line = line;
+		//‰½‚©‚µ‚ç“n‚³‚ê‚½•¶š—ñ‚ğ•\¦‚·‚éB
+		System.out.println(this.line);
+	}
+}
